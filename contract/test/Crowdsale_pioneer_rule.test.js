560777f4830dc8c47038a58c3f050e78a90290a8
Hu Yao-Chieh <yhuag@connect.ust.hk>
2018-09-03 20:09:57 +0800

    update pioneer rule test

--- contract/test/Crowdsale_pioneer_rule.test.js
+++ contract/test/Crowdsale_pioneer_rule.test.js
@@ -44,32 +44,61 @@ contract('Crowdsale', function (accounts) {
             crowdsale = await Crowdsale.new(openingTime, closingTime, pioneerTimeEnd);
         });
 
+        /* [Begin State]
+         * crowdsale deployed
+         * no pioneer
+        */
         it("should have no pioneers at first", async function () {
             assert.equal(await crowdsale.isPioneer(owner), false);
             assert.equal(await crowdsale.isPioneer(buyer1), false);
             assert.equal(await crowdsale.isPioneer(anyone), false);
         });
 
+        /* [Begin State]
+         * crowdsale deployed
+         * no pioneer
+        */
         it("should be a pioneer after paying >= pioneerWeiThreshold", async function () {
             await crowdsale.purchaseTokens(anyone, {from: buyer1, value: pioneerWeiThreshold});
             assert.equal(await crowdsale.isPioneer(buyer1), true);
         });
 
+        /* [Begin State]
+         * crowdsale deployed
+         * pioneer:
+         * - buyer1
+        */
         it("should not be a pioneer after paying (pioneerWeiThreshold / 2)", async function () {
             await crowdsale.purchaseTokens(anyone, {from: buyer2, value: pioneerWeiThreshold.dividedToIntegerBy(2)});
             assert.equal(await crowdsale.isPioneer(buyer2), false);
         });
 
+        /* [Begin State]
+         * crowdsale deployed
+         * pioneer:
+         * - buyer1
+        */
         it("should become a pioneer after paying (pioneerWeiThreshold / 2) twice", async function () {
             await crowdsale.purchaseTokens(anyone, {from: buyer2, value: pioneerWeiThreshold.dividedToIntegerBy(2)});
             assert.equal(await crowdsale.isPioneer(buyer2), true);
         });
 
+        /* [Begin State]
+         * crowdsale deployed
+         * pioneer:
+         * - buyer1
+         * - buyer2
+        */
         it("should not be a pioneer after pioneerTimeEnd", async function () {
             // await crowdsale.setPioneerTimeEnd(latestTime+1, {from: owner});
             await increaseTimeTo(pioneerTimeEnd + 1);
             await crowdsale.purchaseTokens(anyone, {from: buyer3, value: pioneerWeiThreshold});
             assert.equal(await crowdsale.isPioneer(buyer3), false);
+
+            assert.equal(await crowdsale.isPioneer(buyer1), true);
+            assert.equal(await crowdsale.isPioneer(buyer2), true);
+            assert.equal(await crowdsale.isPioneer(owner), false);
+            assert.equal(await crowdsale.isPioneer(anyone), false);
         });
     });
 });
