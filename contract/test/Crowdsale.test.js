631d94bbbac7d3243c40b1c069495d092868048b
Ting Ting Lee <tingtinglee@Tings-MacBook-Pro.local>
2018-09-03 09:13:48 +0800

    test ownership transfer, setter, and event emit

--- contract/test/Crowdsale.test.js
+++ contract/test/Crowdsale.test.js
@@ -1,7 +1,9 @@
+var assertRevert = require('./helper/assertRevert').assertRevert;
 const CrowdSale = artifacts.require("CrowdSale");
 
 let crowdSale;
 let owner;
+let someone;
 let buyer1; 
 
 before(async () => {
@@ -17,17 +19,44 @@ contract('CrowdSale', function (accounts) {
 
     // define roles by accounts 
     owner = accounts[0];
-    buyer1 = accounts[1];
+    someone = accounts[1];
+    buyer1 = accounts[2];
 
     it("test ownership transfer", async function () {
         var _owner = await crowdSale.owner();
         assert.equal(_owner, owner, "owner should be equaled to contract deployer");
 
-        var newOwner = accounts[1];
-        await crowdSale.transferOwnership(newOwner);
-        await crowdSale.claimOwnership({from: newOwner});
+        // transfer ownership to someone 
+        await crowdSale.transferOwnership(someone);
+        await crowdSale.claimOwnership({from: someone});
         var _owner = await crowdSale.owner();
-        assert.equal(_owner, newOwner, "owner should be changed to new owner");
+        assert.equal(_owner, someone, "owner should be changed to new owner");
+
+        // transfer back ownership to original owner
+        await crowdSale.transferOwnership(owner, {from: someone});
+        var result = await crowdSale.claimOwnership({from: owner});
+        var _owner = await crowdSale.owner();
+        assert.equal(_owner, owner, "owner should be changed back to original contract deployer");
+
+        // check ownership tranfer event
+        assert.equal(result.logs[0].event, "OwnershipTransferred");
+        assert.equal(result.logs[0].args.previousOwner.valueOf(), someone);
+        assert.equal(result.logs[0].args.newOwner.valueOf(), owner);
+    });
+
+    it("test setters", async function () {
+        // test setRate
+        var rate = 10;
+        var result = await crowdSale.setRate(rate);
+
+        // check event emit
+        assert.equal(result.logs[0].event, "RateChanged");
+        assert.equal(result.logs[0].args.rate.valueOf(), rate);
+
+        // check someone other than owner 
+        await assertRevert(crowdSale.setRate(rate, {from: someone}), "rate should only be set by owner"); 
+
+        //TODO: add other setter tests
     });
 });
   
