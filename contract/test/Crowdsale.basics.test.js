07a6b18f80ca7cbd9777b31d2245f269c5f27d4b
Ting Ting Lee <tingtinglee@Tings-MacBook-Pro.local>
2018-09-04 21:10:37 +0800

    ERROR

--- contract/test/Crowdsale.basics.test.js
+++ contract/test/Crowdsale.basics.test.js
@@ -1,46 +1,52 @@
 const { assertRevert } = require("./helper/assertRevert");
-const CrowdSale = artifacts.require("CrowdSale");
+const { ether } = require("./helper/ether");
+const { expectThrow } = require("./helper/expectThrow");
+const { latestTime } = require("./helper/latestTime");
+const { increaseTime, increaseTimeTo } = require("./helper/increaseTime");
+const CrowdSale = artifacts.require("Crowdsale");
+
+const BigNumber = web3.BigNumber;
+
+const should = require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
 
 let crowdSale;
-let owner;
-let someone;
-let buyer1; 
 
 before(async () => {
     // open and end time is not real case here, so we are not testing onlyWhileOpen here
-    var openingTime = Date.now();
-    var closingTime = openingTime + 5*60; // 5 mins after opening time 
-    var pioneerTimeEnd = closingTime;
+    const timestep = 86400;
+    const openingTime = Math.round(Date.now()/1000) -1000;
+    const closingTime = openingTime + timestep * 2;
+    const pioneerTimeEnd = openingTime + timestep;
     crowdSale = await CrowdSale.new(openingTime, closingTime, pioneerTimeEnd); 
 });
 
 
 contract('CrowdSale', function (accounts) {
 
-    // define roles by accounts 
-    owner = accounts[0];
-    someone = accounts[1];
-    buyer1 = accounts[2];
+    const [owner, buyer1, buyer2, buyer3, anyone] = accounts;
 
     it("test ownership transfer", async function () {
         var _owner = await crowdSale.owner();
         assert.equal(_owner, owner, "owner should be equaled to contract deployer");
 
-        // transfer ownership to someone 
-        await crowdSale.transferOwnership(someone);
-        await crowdSale.claimOwnership({from: someone});
+        // transfer ownership to anyone 
+        await crowdSale.transferOwnership(anyone);
+        await crowdSale.claimOwnership({from: anyone});
         var _owner = await crowdSale.owner();
-        assert.equal(_owner, someone, "owner should be changed to new owner");
+        assert.equal(_owner, anyone, "owner should be changed to new owner");
 
         // transfer back ownership to original owner
-        await crowdSale.transferOwnership(owner, {from: someone});
+        await crowdSale.transferOwnership(owner, {from: anyone});
         var result = await crowdSale.claimOwnership({from: owner});
         var _owner = await crowdSale.owner();
         assert.equal(_owner, owner, "owner should be changed back to original contract deployer");
 
         // check ownership tranfer event
         assert.equal(result.logs[0].event, "OwnershipTransferred");
-        assert.equal(result.logs[0].args.previousOwner.valueOf(), someone);
+        assert.equal(result.logs[0].args.previousOwner.valueOf(), anyone);
         assert.equal(result.logs[0].args.newOwner.valueOf(), owner);
     });
 
@@ -53,8 +59,8 @@ contract('CrowdSale', function (accounts) {
         assert.equal(result.logs[0].event, "RateChanged");
         assert.equal(result.logs[0].args.rate.valueOf(), rate);
 
-        // check someone other than owner 
-        await assertRevert(crowdSale.setRate(rate, {from: someone}), "rate should only be set by owner"); 
+        // check anyone other than owner 
+        await assertRevert(crowdSale.setRate(rate, {from: anyone}), "rate should only be set by owner"); 
 
         //TODO: add other setter tests
     });
@@ -71,8 +77,8 @@ contract('CrowdSale', function (accounts) {
         // withdraw money that exceeds total contract balance 
         await assertRevert(crowdSale.withdraw(contractBalance + 10), "cannot withdraw money that exceeds total contract balance"); 
 
-        // check someone other than owner 
-        await assertRevert(crowdSale.withdraw(contractBalance, {from: someone}), "only owner should be able to withdraw"); 
+        // check anyone other than owner 
+        await assertRevert(crowdSale.withdraw(contractBalance, {from: anyone}), "only owner should be able to withdraw"); 
     });
 
     it("test pause", async function (){
@@ -91,5 +97,14 @@ contract('CrowdSale', function (accounts) {
         paused = await crowdSale.paused();
         assert.equal(paused, false, "pause should be set to false when unpaused");
     });
+
+    it("test direct send money to contract", async function() {
+        await web3.eth.sendTransaction({to:crowdSale.address, from:buyer2, value: ether(0.5)});
+
+        let amountToBuyWei = ether(0.5);
+        let tokens = amountToBuyWei.times(rate);
+
+        (await crowdsale.balanceOf(buyer2)).should.be.bignumber.equal( tokens);
+    })
 });
   
